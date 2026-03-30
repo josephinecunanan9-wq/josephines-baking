06580c2612d8556389b0a6d5619527cbbb1ae721
josephinecunanan9-wq <josephinecunanan9@gmail.com>
2026-03-30 20:20:08 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,5 @@
 import '../styles/globals.css'
-import Newsletter from '../components/Newsletter'
-import Footer from '../components/Footer'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Newsletter />
-      <Footer />
-    </>
-  )
+  return <Component {...pageProps} />
 }
