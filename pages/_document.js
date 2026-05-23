0e89e69d419b824c6f676d7ae05504e2ba66a449
josephinecunanan9-wq <josephinecunanan9@gmail.com>
2026-05-23 15:45:12 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" href="/favicon.ico" />
         <meta name="google-site-verification" content="b5CiN4dnkVS4r_IpzYm3dwuGUD-wGZTBmu-JkQgPkfQ" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
